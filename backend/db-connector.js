860561db5ed9350b5ef5eb0dba791c0487c39103
Lucca James Truitt <truittl@oregonstate.edu>
2026-03-02 18:51:01 -0800

    Updated db-connector.js
    Added Citation and Removed ONID and Password

--- backend/db-connector.js
+++ backend/db-connector.js
@@ -1,3 +1,8 @@
+// Citation for the following code:
+// Date: 3/2/2026
+// Copied from
+// Source URL: https://canvas.oregonstate.edu/courses/2031764/assignments/10323319?module_item_id=26243357
+
 // Get an instance of mysql we can use in the app
 const mysql = require("mysql2");
 
@@ -6,9 +11,9 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_etzelmik', // example: cs340_MyOnidIsBilboBaggins
-    password: 'qN05AdlEajv5', // last 4 of your OSU ID number
-    database: 'cs340_etzelmik' // should be same as user
+    user: 'cs340_[onid]', // example: cs340_MyOnidIsBilboBaggins
+    password: '[password]', // last 4 of your OSU ID number
+    database: 'cs340_[onid]' // should be same as user
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
