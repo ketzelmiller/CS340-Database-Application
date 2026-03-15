25025eeb74b7ce25a96721de51c293414be58037
Lucca James Truitt <truittl@oregonstate.edu>
2026-03-15 04:02:15 -0700

    Update BranchesPage.jsx
    Changed Edit Button functionality. There is a now a second form below insert. To edit a branch, insert data into this form, then click the update button next to the row of data your wish to update.

--- frontend/src/pages/BranchesPage.jsx
+++ frontend/src/pages/BranchesPage.jsx
@@ -12,8 +12,11 @@ function BranchesPage(){
   const [branchName, setBranchName] = useState("")
   const [city, setCity] = useState("")
   const [state, setState] = useState("")
+  const [updateBranchName, setUpdateBranchName] = useState("")
+  const [updateCity, setUpdateCity] = useState("")
+  const [updateState, setUpdateState] = useState("")
 
-  const backend = "http://classwork.engr.oregonstate.edu:6044"
+  const backend = "http://classwork.engr.oregonstate.edu:6098"
   //const backend = "http://localhost:3001"
 
   // --- LOAD ---
@@ -81,9 +84,9 @@ function BranchesPage(){
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          branchName: branchName,
-          city: city,
-          state: state
+          branchName: updateBranchName,
+          city: updateCity,
+          state: updateState
         })
       })
 
@@ -93,9 +96,9 @@ function BranchesPage(){
 
       // Refresh table and clear fields
       await loadBranches();
-      setBranchName("");
-      setCity("")
-      setState("")
+      setUpdateBranchName("");
+      setUpdateCity("")
+      setUpdateState("")
 
     }catch(err){
       console.log(err)
@@ -230,6 +233,95 @@ function BranchesPage(){
         </label>
         <br></br>
         <button style={{marginTop: '7px', padding:'8px'}} type="submit">Add Branch</button>
+      </form>
+
+      <h2>Modify Branch</h2>
+      <p>To Modify a Branch, enter data in the form below, then click the 'update' button <br></br> next to the row you wish to modify</p>
+      <form>
+        <input 
+          style={{padding:'7px'}} 
+          type="text" 
+          name="branchName" 
+          required="required" 
+          placeholder="Enter branch name" 
+          value={updateBranchName} 
+          onChange={(e) => setUpdateBranchName(e.target.value)}
+        />
+        <br></br>
+        <input 
+          style={{marginTop: '7px', marginBottom: '6px', padding:'7px'}} 
+          type="text" 
+          name="city" 
+          required="required" 
+          placeholder="Enter city" 
+          value={updateCity} 
+          onChange={(e) => setUpdateCity(e.target.value)}
+        />
+        <br></br>
+        <label>
+          Select State:
+          <select 
+            style={{marginLeft:'7px'}} 
+            name="selectedState"
+            value = {updateState}
+            onChange={(e) => setUpdateState(e.target.value)}>
+            <option value="">Select</option>
+            <option value="AL">AL</option>
+            <option value="AK">AK</option>
+            <option value="AZ">AZ</option>
+            <option value="AR">AR</option>
+            <option value="CA">CA</option>
+            <option value="CO">CO</option>
+            <option value="CT">CT</option>
+            <option value="DE">DE</option>
+            <option value="DC">DC</option>
+            <option value="FL">FL</option>
+            <option value="GA">GA</option>
+            <option value="HI">HI</option>
+            <option value="ID">ID</option>
+            <option value="IL">IL</option>
+            <option value="IN">IN</option>
+            <option value="IA">IA</option>
+            <option value="KS">KS</option>
+            <option value="KY">KY</option>
+            <option value="LA">LA</option>
+            <option value="ME">ME</option>
+            <option value="MD">MD</option>
+            <option value="MA">MA</option>
+            <option value="MI">MI</option>
+            <option value="MN">MN</option>
+            <option value="MS">MS</option>
+            <option value="MO">MO</option>
+            <option value="MT">MT</option>
+            <option value="NE">NE</option>
+            <option value="NV">NV</option>
+            <option value="NH">NH</option>
+            <option value="NJ">NJ</option>
+            <option value="NM">NM</option>
+            <option value="NY">NY</option>
+            <option value="NC">NC</option>
+            <option value="ND">ND</option>
+            <option value="OH">OH</option>
+            <option value="OK">OK</option>
+            <option value="OR">OR</option>
+            <option value="PA">PA</option>
+            <option value="PR">PR</option>
+            <option value="RI">RI</option>
+            <option value="SC">SC</option>
+            <option value="SD">SD</option>
+            <option value="TN">TN</option>
+            <option value="TX">TX</option>
+            <option value="UT">UT</option>
+            <option value="VT">VT</option>
+            <option value="VI">VI</option>
+            <option value="VA">VA</option>
+            <option value="WA">WA</option>
+            <option value="WV">WV</option>
+            <option value="WI">WI</option>
+            <option value="WY">WY</option>
+          </select>
+        </label>
+        <br></br>
       </form>
     </div>
   )
